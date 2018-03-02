9ea704fadb6dace2e655fe3c9e2c13cb558e8c0b
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2018-03-01 20:34:44 -0500

    delete zip data in branch

--- lib/feature.R
+++ lib/feature.R
@@ -2,38 +2,28 @@
 ### Construct visual features for training/testing images ###
 #############################################################
 
-### Authors: Yuting Ma/Tian Zheng
-### Project 3
-### ADS Spring 2017
+### Authors: Ginny Gao
+### Project 2
+### ADS Spring 2018
+
+#SIFT feature, first try: select row with max keypoint feature sum
 
 feature <- function(img_dir, set_name, data_name="data", export=T){
-  
-  ### Construct process features for training/testing images
-  ### Sample simple feature: Extract row average raw pixel values as features
-  
-  ### Input: a directory that contains images ready for processing
-  ### Output: an .RData file contains processed features for the images
-  
-  ### load libraries
-  library("EBImage")
-  
-  n_files <- length(list.files(img_dir))
-  
-  ### determine img dimensions
-  img0 <-  readImage(paste0(img_dir, "img", "_", data_name, "_", set_name, "_", 1, ".jpg"))
-  mat1 <- as.matrix(img0)
-  n_r  <- nrow(img0)
-  
-  ### store vectorized pixel values of images
-  dat <- matrix(NA, n_files, n_r) 
-  for(i in 1:n_files){
-    img     <- readImage(paste0(img_dir,  "img", "_", data_name, "_", set_name, "_", i, ".jpg"))
-    dat[i,] <- rowMeans(img)
-  }
-  
-  ### output constructed features
-  if(export){
+
+train_features_dir <- paste(experiment_dir, "train-features/", sep = "")
+
+n_files <- length(list.files(img_dir))
+
+dat <- matrix(NA, n_files, 128)
+for(i in 1 : n_files){
+    load(paste(train_features_dir, 'pet', i, '.jpg.sift.Rdata', sep = ''))
+    dat[i,] <- subset(features, rowSums(features) == max(rowSums(features)))
+    }
+
+### output constructed features
+if(export){
     save(dat, file = paste0("../output/feature_", data_name, "_", set_name, ".RData"))
-  }
-  return(dat)
+    }
+return(dat)
 }
+
