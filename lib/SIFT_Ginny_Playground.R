e76edcae6c0f631b63a617293409213d89c9f9aa
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2018-03-01 21:39:31 -0500

    sift add max, min branch

--- lib/SIFT_Ginny_Playground.R
+++ lib/SIFT_Ginny_Playground.R
@@ -1,29 +1,38 @@
-#SIFT max, min
+#############################################################
+### Construct visual features for training/testing images ###
+#############################################################
 
-dat <- matrix(NA, 4, 128)
+### Authors: Ginny Gao
+### Project 2
+### ADS Spring 2018
+
+#SIFT features with max, min sum per image
+
+
+feature <- function(img_dir, set_name, data_name = "data", export = T){
+
+train_features_dir <- paste(experiment_dir, "train-features/", sep = "")
+
+n_files <- length(list.files(img_dir))
+
 load(paste(train_features_dir, 'pet', 1, '.jpg.sift.Rdata', sep = ''))
-dat1 <- rbind(subset(features, rowSums(features) == max(rowSums(features))), 
-		#subset(features, rowSums(features) == median(rowSums(features))),
-    	subset(features, rowSums(features) == min(rowSums(features))))
-for(i in 2 : 2){
-    load(paste(train_features_dir, 'pet', i, '.jpg.sift.Rdata', sep = ''))
-    dat[1:2,] <- rbind(subset(features, rowSums(features) == max(rowSums(features))),
-    	#subset(features, rowSums(features) == median(rowSums(features))),
-    	subset(features, rowSums(features) == min(rowSums(features))))
-	dat <- rbind(dat1, dat[1:2,])
+dat <- rbind(subset(features, rowSums(features) == max(rowSums(features))), 
+             subset(features, rowSums(features) == min(rowSums(features))))
+
+for (i in 2 : n_files) {
+    	load(paste(train_features_dir, 'pet', i, '.jpg.sift.Rdata', sep = ''))
+    	dat_i[1 : 2, ] <- rbind(subset(features, rowSums(features) == max(rowSums(features))),
+    	                        subset(features, rowSums(features) == min(rowSums(features))))
+    	dat <- rbind(dat, dat_i[1 : 2, ])
 	}
 
 
-
-
-
-
-
-
-
-
-
-
+### output constructed features
+if(export){
+    save(dat, file = paste0("../output/feature_", data_name, "_", set_name, ".RData"))
+    }
+return(dat)
+}
 
 
 
