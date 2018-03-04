058141e464333a4e9ed2c315d0d3b2fa48271dcb
Ginny Gao <qinqingao1a@hotmail.com>
2018-03-04 13:16:32 -0500

    update feature.R with test_features_dir

--- lib/feature.R
+++ lib/feature.R
@@ -11,7 +11,19 @@
 
 feature <- function(img_dir, set_name, data_name = "data", export = T){
 
+######################### CHANGE THIS VARIABLE #########################
+#set the starting pet image number in test_features_dir
+
+test_start = 1601
+
+########################################################################
+
+
 train_features_dir <- paste(experiment_dir, "train-features/", sep = "")
+
+test_features_dir <- paste(experiment_dir, "test-features/", sep = "")
+
+if (img_dir == img_train_dir) {
 
 n_files <- length(list.files(img_dir))
 
@@ -48,6 +60,47 @@ for (i in 2 : n_files) {
                                 t(bof$centers[10, ])
     	                        ))
 	}
+}
+
+else {
+
+n_files <- length(list.files(img_dir))
+
+load(paste(test_features_dir, 'pet', test_start, '.jpg.sift.Rdata', sep = ''))
+
+set.seed(1234)
+bof <- kmeans(features, 20, iter.max = 20)
+
+dat <- cbind(t(bof$centers[1, ]), 
+             t(bof$centers[2, ]),
+             t(bof$centers[3, ]),
+             t(bof$centers[4, ]),
+             t(bof$centers[5, ]),
+             t(bof$centers[6, ]),
+             t(bof$centers[7, ]),
+             t(bof$centers[8, ]),
+             t(bof$centers[9, ]),
+             t(bof$centers[10, ])
+)
+
+for (i in (test_start + 1) : (test_start - 1 + n_files)) {
+        load(paste(test_features_dir, 'pet', i, '.jpg.sift.Rdata', sep = ''))
+        set.seed(1234)
+        bof <- kmeans(features, 20, iter.max = 20)
+        dat <- rbind(dat, cbind(t(bof$centers[1, ]),
+                                t(bof$centers[2, ]),
+                                t(bof$centers[3, ]),
+                                t(bof$centers[4, ]),
+                                t(bof$centers[5, ]),
+                                t(bof$centers[6, ]),
+                                t(bof$centers[7, ]),
+                                t(bof$centers[8, ]),
+                                t(bof$centers[9, ]),
+                                t(bof$centers[10, ])
+                                ))
+    }
+
+}
 
 
 ### output constructed features
