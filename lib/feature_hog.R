c5d103388a0fc7a88cce1b045d4e9b5ceabe08c9
Juho Ma <35592552+juhoma@users.noreply.github.com>
2018-03-05 10:48:46 -0500

    Add files via upload

---
+++ lib/feature_hog.R
@@ -0,0 +1,31 @@
+feature_hog <- function(img_dir, set_name, data_name = "pet", export = TRUE){
+	
+	# Load necessary library
+	library(reticulate)
+	cv2 <- reticulate::import('cv2')
+	
+	# Set parameter values
+	winSize <- tuple(64L,64L)
+	blockSize <- tuple(16L,16L)
+	blockStride <- tuple(8L,8L)
+	cellSize <- tuple(8L,8L)
+	nbins <- 9L	
+	hog <- cv2$HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins)
+	
+	# Create a feature matrix
+	n_files <- length(list.files(img_dir))
+	dat <- matrix(NA, n_files, 1764)
+
+	for (i in 1:n_files){
+		img <- readImage(paste0(img_dir, data_name, i, ".jpg"))
+		img_resized <- cv2$resize(img, dsize=tuple(64L, 64L))
+		hog_values <- hog$compute(np_array(img_resized * 255, dtype='uint8'))
+		dat[i,] <- hog_values
+	}
+	
+	# Export and save the output
+	if (export){
+		save(dat, file=paste0("../output/feature_", data_name, "_", set_name, ".RData"))
+	}
+	return(dat)
+}
