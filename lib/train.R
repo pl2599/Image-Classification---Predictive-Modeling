dd9682048f6915d0266fa7dbeb2f73328513e7a1
ginnyqg <21312923+ginnyqg@users.noreply.github.com>
2018-03-05 09:44:17 -0500

    github clean up

--- lib/train.R
+++ lib/train.R
@@ -26,7 +26,7 @@ train <- function(dat_train, label_train, par = NULL){
     depth <- par$depth
   }
   fit_gbm <- gbm.fit(x = dat_train, y = label_train,
-                     n.trees = 500,
+                     n.trees = 2000,
                      distribution = "bernoulli",
                      interaction.depth = depth, 
                      bag.fraction = 0.5,
